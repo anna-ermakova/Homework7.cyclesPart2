3944887980f597675cb8b4d4918c920009269b30
anna-ermakova <korovanna8@mail.ru>
2022-10-08 09:55:57 +0300

    ДЗ7. Циклы. часть 2. Задача1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Циклы. часть 2
+
+        //task1
+        //С помощью цикла while посчитайте, сколько месяцев потребуется, чтобы накопить 2 459 000 рублей при условии,
+        // что первоначально мы имеем 0 рублей и готовы откладывать по 15 тысяч рублей.
+        System.out.println("Задание 1.");
+        int amount=15_000;
+        int total=0;
+        int i=0;
+        while (total <=2_459_000) {
+            total = total + total / 100;
+            total = total + amount;
+            total++;
+            i++;
+            System.out.println("Месяц " + i + " , сумма накоплений равна " + total + " рублей.");
+        }
+        }
     }
-}
